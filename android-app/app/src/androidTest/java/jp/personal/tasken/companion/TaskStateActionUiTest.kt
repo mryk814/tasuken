0551441c5d3b3c47569c86de34fccfeaf4ddb653
mryk814 <moriyuki.814@gmail.com>
2026-08-22 04:15:41 +0900

    test(android): record title edit device evidence

--- android-app/app/src/androidTest/java/jp/personal/tasken/companion/TaskStateActionUiTest.kt
+++ android-app/app/src/androidTest/java/jp/personal/tasken/companion/TaskStateActionUiTest.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextReplacement
 import org.junit.Assert.assertEquals
