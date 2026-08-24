6e310131f4e3b70b292d35175251bc4c11f6a34d
mryk814 <moriyuki.814@gmail.com>
2026-08-24 18:08:46 +0900

    fix(android): import cached pane layout

--- android-app/app/src/main/java/jp/personal/tasken/companion/MainActivity.kt
+++ android-app/app/src/main/java/jp/personal/tasken/companion/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
